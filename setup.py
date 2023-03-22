3cfc9aa38d4ba2488d1883e0e1521b6386a1736a
Rémy Le Calloch <remy@lecalloch.net>
2023-03-22 13:59:20 +0100

    Fix: invalid module name

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if root_dir:
     os.chdir(root_dir)
 accepted_filetypes = (".html", ".png", ".svg", ".js", ".css")
 
-for dirpath, dirnames, filenames in os.walk('ab-plugin-notebook'):
+for dirpath, dirnames, filenames in os.walk('ab_plugin_notebook'):
     # Ignore dirnames that start with '.'
     if ('__init__.py' in filenames
             or any(x.endswith(accepted_filetypes) for x in filenames)):
@@ -21,7 +21,7 @@ if 'VERSION' in os.environ:
     version = os.environ['VERSION']
 else:
     version = os.environ.get('GIT_DESCRIBE_TAG', '0.0.0')
-print(packages)
+
 setup(
     name='ab-plugin-notebook',
     version=version,
@@ -34,21 +34,4 @@ setup(
     url='https://github.com/Pan6ora/ab-plugin-Notebook',
     long_description=open('README.md').read(),
     description='Launch Jupyter Notebook directly in Activity-Browser',
-    classifiers=[
-        'Intended Audience :: End Users/Desktop',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Topic :: Scientific/Engineering :: Information Analysis',
-        'Topic :: Scientific/Engineering :: Mathematics',
-        'Topic :: Scientific/Engineering :: Visualization',
-    ],
     )
